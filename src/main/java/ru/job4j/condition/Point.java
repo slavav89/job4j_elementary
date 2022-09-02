2269d1dd6342544abd4bff10a8c4aca4cce50ab7
Вячеслав <slava_zen@mail.ru>
2022-09-02 15:14:07 +0500

    ru.job4j.condition.Point

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -3,12 +3,7 @@ package ru.job4j.condition;
 public class Point {
 
     public static double distance(int x1, int y1, int x2, int y2) {
-        double a = x2 - x1;
-        double b = y2 - y1;
-        double c = Math.pow(a, 2);
-        double d = Math.pow(b, 2);
-        double e = c + d;
-        return Math.sqrt(e);
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 
     }
 
