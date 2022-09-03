6b524a9107939e6102dff233cb7781405e4fc3f8
Вячеслав <slava_zen@mail.ru>
2022-09-03 11:37:50 +0500

    ru.job4j.calculator.MathCalculator

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double diffAndDiv(double first, double second) {
+        return diff(first, second)
+                + div(first, second);
+    }
+
+    public static double sumAndMultiplyAndDiffandDiv(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second)
+                + diff(first, second)
+                + div(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета сложения суммы и умножения равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета сложения разности и деления равен:" + diffAndDiv(10, 20));
+        System.out.println("Результат расчета сложения суммы, умножения, разности и деления равен:" + sumAndMultiplyAndDiffandDiv(10, 20));
+    }
+}
