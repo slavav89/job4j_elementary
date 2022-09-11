7ae497034418d9d2e4a7f367e7396bdbb524586b
Вячеслав <slava_zen@mail.ru>
2022-09-11 22:33:08 +0500

    ru.job4j.condition.ChessBoard

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -5,7 +5,7 @@ public class ChessBoard {
         int rsl = 0;
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
-            if (x1 != x2 && y1 != y2 && Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
+            if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
                 rsl = Math.abs(x2 - x1);
             }
         }
