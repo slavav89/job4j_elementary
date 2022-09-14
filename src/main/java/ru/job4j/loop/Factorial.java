3f8dbcd967e9648ec5e7361154ae07cda6a3f04f
Вячеслав <slava_zen@mail.ru>
2022-09-14 17:05:57 +0500

    ru.job4j.loop.Factorial

---
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public static int calc(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            if (i == 0) {
+                result = 1;
+            } else {
+                result = result * i;
+            }
+        }
+        return result;
+    }
+}
