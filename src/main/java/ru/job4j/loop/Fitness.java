15806ade8ce692dd48cc7f605e62e52954a427d1
Вячеслав <slava_zen@mail.ru>
2022-09-17 14:24:14 +0500

    ru.jo4j.loop.Fitness

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -1,5 +1,4 @@
 package ru.job4j.loop;
-
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
@@ -9,5 +8,9 @@ public class Fitness {
             month++;
         }
         return month;
+    }
+    public static void main(String[] args) {
+        int srl = Fitness.calc(100, 290);
+        System.out.println(srl);
     }
 }
