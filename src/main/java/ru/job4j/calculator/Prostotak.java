a25810b3371352ca744d0681a0b1b5c96c8bba5c
Вячеслав <slava_zen@mail.ru>
2022-09-17 18:01:28 +0500

    ru.jo4j.loop.Prostotak

--- src/main/java/ru/job4j/calculator/Prostotak.java
+++ src/main/java/ru/job4j/calculator/Prostotak.java
@@ -1,13 +1,7 @@
 package ru.job4j.calculator;
 
-import java.util.Scanner;
-
 public class Prostotak {
     public static void main(String[] args) {
-        //Scanner scan = new Scanner(System.in);
-        //System.out.println("Введите ваше имя: ");
-        //String username = scan.nextLine();
-        //System.out.println(username);
         short s = 1500;
         char c = (char) s;
         double d = c;
