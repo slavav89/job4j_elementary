9466c3afffa5dcc9e9c10bcd31b7963a9fb48190
Вячеслав <slava_zen@mail.ru>
2022-09-24 20:49:57 +0500

    ru.jo4j.array.EndsWith

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -3,9 +3,8 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
-        int x = word.length - post.length;
         for (int i = 0; i < post.length; i++) {
-            if (word[x + i] != post[i]) {
+            if (word[word.length - post.length + i] != post[i]) {
                 result = false;
                 break;
             }
