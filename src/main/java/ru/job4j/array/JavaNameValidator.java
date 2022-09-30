e31e65b44e7e141d9ae9896b303cf00e8574b824
Вячеслав <slava_zen@mail.ru>
2022-09-30 20:22:41 +0500

    ru.jo4j.array.JavaNameValidator

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -2,40 +2,38 @@ package ru.job4j.array;
 
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        boolean valid = false;
-        for (int i = 0; i < name.length(); i++) {
-            int code = name.codePointAt(i);
-            if (isUpperCase(name)
-                    && isEmpty(name)
-                    && (isSpecialSymbol(code)
-                    || isUpperLatinLetter(code)
-                    || isLowerLatinLetter(code)
-                    || Character.isDigit(i))) {
-                valid = true;
-                break;
+        char[] array = name.toCharArray();
+        boolean valid = !name.isEmpty() && isUpperCase(name) && !Character.isDigit(array[0]);
+        if (valid) {
+            for (int i = 1; i < name.length(); i++) {
+                int code = name.codePointAt(i);
+                if (isSpecialSymbol(code)
+                        && isUpperLatinLetter(code)
+                        && isLowerLatinLetter(code)
+                        && Character.isDigit(i)) {
+                    valid = false;
+                    break;
+                }
             }
         }
         return valid;
+
     }
 
     public static boolean isSpecialSymbol(int code) {
-        return (code != 36 && code != 95);
+        return (code == 36 || code == 95);
     }
 
     public static boolean isUpperLatinLetter(int code) {
-        return (65 <= code && code <= 90);
+        return (65 > code || code > 90);
     }
 
     public static boolean isLowerLatinLetter(int code) {
-        return (97 <= code && code <= 122);
+        return (97 > code || code > 122);
     }
 
     public static boolean isUpperCase(String name) {
         return !Character.isUpperCase(name.charAt(0));
-    }
-
-    public static boolean isEmpty(String name) {
-        return !name.isEmpty();
     }
 
 }
